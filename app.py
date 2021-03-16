ecd27515ec0ef867f731c1b0c60f690c65dc7e66
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-03-16 13:03:05 -0400

    loads data from csv

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Hello world!")
+st.title("Airline Sentiment Analysis")
+st.sidebar.title("Sentiment Analysis of Tweets about Airlines")
+
+st.markdown("This application is a Streamlit dashboard to analyze the sentiment of Tweets about US airlines")
+st.sidebar.markdown("This application is a Streamlit dashboard to analyze the sentiment of Tweets about US airlines")
+
+DATA_URL = "Tweets.csv"
+
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    data["tweet_created"] = pd.to_datetime(data["tweet_created"])
+    return data
+
+
+data = load_data()
