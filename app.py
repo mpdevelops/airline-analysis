8c3538f07f2c816202c03d076950909982980ce4
Mike Paiva <mikepaiva@resnet300-passy-ip-10-36-33.153.resnet.yorku.ca>
2021-03-16 13:17:56 -0400

    display data as bar and pie charts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 st.title("Airline Sentiment Analysis")
 st.sidebar.title("Sentiment Analysis of Tweets about Airlines")
@@ -23,3 +24,17 @@ data = load_data()
 st.sidebar.subheader("Show random Tweet")
 random_tweet = st.sidebar.radio("Sentiment", ("positive", "neutral", "negative"))
 st.sidebar.markdown(data.query("airline_sentiment == @random_tweet")[["text"]].sample(n=1).iat[0, 0])
+
+st.sidebar.markdown("### Number of Tweets by sentiment")
+select = st.sidebar.selectbox("Visualization type", ["Histogram", "Pie Chart"], key="1")
+sentiment_count = data['airline_sentiment'].value_counts()
+sentiment_count = pd.DataFrame({"Sentiment": sentiment_count.index, "Tweets": sentiment_count.values})
+
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Number of Tweets by sentiment")
+    if select == "Histogram":
+        fig = px.bar(sentiment_count, x="Sentiment", y="Tweets", color="Tweets", height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiment_count, values="Tweets", names="Sentiment")
+        st.plotly_chart(fig)
