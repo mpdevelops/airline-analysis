bf49e1927b29ad254f0b11489170e289014b17a2
Mike Paiva <49703846+mpdevelops@users.noreply.github.com>
2021-04-02 11:32:08 -0400

    fixed toggles

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ select = st.sidebar.selectbox("Visualization type:", ["Histogram", "Pie Chart"],
 sentiment_count = data['airline_sentiment'].value_counts()
 sentiment_count = pd.DataFrame({"Sentiment": sentiment_count.index, "Tweets": sentiment_count.values})
 
-if not st.sidebar.checkbox("Toggle Display", True):
+if not st.sidebar.checkbox("Toggle Display", False):
     st.markdown("### Number of Tweets by sentiment")
     if select == "Histogram":
         fig = px.bar(sentiment_count, x="Sentiment", y="Tweets", color="Tweets", height=500)
@@ -42,7 +42,7 @@ if not st.sidebar.checkbox("Toggle Display", True):
 st.sidebar.subheader("When and where are users tweeting from?")
 hour = st.sidebar.slider("Hour of day", 0, 23)
 modified_data = data[data["tweet_created"].dt.hour == hour]
-if not st.sidebar.checkbox("Toggle Display", True, key="1"):
+if not st.sidebar.checkbox("Toggle Display", False, key="1"):
     st.markdown("### Tweets locations based on the time of day")
     st.markdown("%i Tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour + 1) % 24))
     st.map(modified_data)
@@ -64,7 +64,7 @@ if len(choice) > 0:
 st.sidebar.header("Word Cloud")
 word_sentiment = st.sidebar.radio("Display Word Cloud for which sentiment?", ("positive", "neutral", "negative"))
 
-if not st.sidebar.checkbox("Toggle Display", True, key="3"):
+if not st.sidebar.checkbox("Toggle Display", False, key="3"):
     st.header("Word Cloud for %s sentiment" % word_sentiment)
     df = data[data['airline_sentiment'] == word_sentiment]
     words = ' '.join(df["text"])
